b3fe88328b0848b8e1b584966e518acf3c7e97cc
sun30812 <orgsun30812@icloud.com>
2026-05-05 19:05:45 +0900

    테스트 코드 수정

--- test/model_test.dart
+++ test/model_test.dart
@@ -5,49 +5,47 @@ import 'package:tera_food/types/food_preference.dart';
 
 /// [RestaurantProvider]의 기능을 검증하는 단위 테스트.
 ///
-/// - 특정 음식 종료에 대한 음식점 목록 정상 반환여부
+/// - 특정 음식 종류에 대한 음식점 목록 정상 반환여부
 /// - 모든 음식점 목록 정상 반환여부
 void main() {
   test('조건에 맞는 음식점 목록을 정상적으로 가져오는지 테스트', () async {
     RestaurantProvider provider = await RestaurantProvider.getSampleInstance();
-    expect(provider.foods(FoodKind.etc).first.foodName, 'CU');
-    expect(provider.foods(FoodKind.etc).length, 1);
+    expect(provider
+        .foods(FoodKind.etc)
+        .first
+        .foodKind, FoodKind.etc);
+    expect(provider
+        .foods(FoodKind.noodle)
+        .first
+        .foodKind, FoodKind.noodle);
   });
 
   test('모든 음식점 목록을 정상적으로 가져오는지 테스트', () async {
     RestaurantProvider provider = await RestaurantProvider.getSampleInstance();
-    expect(
-      provider.foods(FoodKind.all).any((element) => element.foodName == 'CU'),
-      true,
-    );
-    expect(
-      provider
-          .foods(FoodKind.all)
-          .any((element) => element.foodName == '한솥 도시락'),
-      true,
-    );
-    expect(
-      provider
-          .foods(FoodKind.all)
-          .any((element) => element.foodName == '고스락 칼국수'),
-      true,
-    );
-    expect(
-      provider
-          .foods(FoodKind.all)
-          .any((element) => element.foodName == '뉴욕 버거'),
-      true,
-    );
-    expect(
-      provider.foods(FoodKind.all).any((element) => element.foodName == '명백집'),
-      true,
-    );
-    expect(
-      provider
-          .foods(FoodKind.all)
-          .any((element) => element.foodName == '포케올데이'),
-      true,
-    );
+    expect(provider
+        .foods(FoodKind.all)
+        .where((food) => food.foodKind == FoodKind.bread)
+        .isNotEmpty, true);
+    expect(provider
+        .foods(FoodKind.all)
+        .where((food) => food.foodKind == FoodKind.noodle)
+        .isNotEmpty, true);
+    expect(provider
+        .foods(FoodKind.all)
+        .where((food) => food.foodKind == FoodKind.etc)
+        .isNotEmpty, true);
+    expect(provider
+        .foods(FoodKind.all)
+        .where((food) => food.foodKind == FoodKind.rice)
+        .isNotEmpty, true);
+    expect(provider
+        .foods(FoodKind.all)
+        .where((food) => food.foodKind == FoodKind.soup)
+        .isNotEmpty, true);
+    expect(provider
+        .foods(FoodKind.all)
+        .where((food) => food.foodKind == FoodKind.salad)
+        .isNotEmpty, true);
   });
 
   test('선호도 업데이트 기능이 정상적으로 작동하는지 테스트', () async {
